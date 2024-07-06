5a624908340043914e8a6cb71c838dabfa8c99be
KGBeneby <167141463+KGBeneby@users.noreply.github.com>
2024-07-06 14:13:01 +0000

    kgb-counter-completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,23 @@
 import React from "react";
+import SimpleCounter from "./simpleCounter";
+import calculateSeconds from "./library.js/libraryTime";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = ({ counter }) => {
+  return (
+    <div className="text-center">
+      <SimpleCounter
+        hundredThousands={calculateSeconds(counter, 100000)}
+        tenThousands={calculateSeconds(counter, 10000)}
+        thousands={calculateSeconds(counter, 1000)}
+        hundreds={calculateSeconds(counter, 100)}
+        tens={calculateSeconds(counter, 10)}
+        ones={calculateSeconds(counter, 1)}
+      />
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -9,5 +9,9 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+let counter = 0;
+setInterval(function () {
+  ReactDOM.render(<Home counter={counter} />, document.querySelector("#app"));
+  counter++;
+  console.log(counter);
+}, 1000);
